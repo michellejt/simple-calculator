31270bbecc4e0f79c7e5b145d1f88ec10063428a
michellejt <mtongas@gmail.com>
2023-04-02 01:57:47 +1030

    app

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const buttons = document.querySelectorAll('button')
 const screenDisplay = document.querySelector('.screen')
 let accumulativeCalculation
 let calculation = []
+let themes = document.querySelector('.msg')
 
 //callback for button
 function calculate(button) {
@@ -19,4 +20,55 @@ function calculate(button) {
     }
 }
 
-buttons.forEach(button => button.addEventListener('click', () => calculate(button)))
+// Listen for all focus events in the document
+/* theme.forEach(item => {
+    item.addEventListener('click', event => {
+     item.classList.toggle('.color');
+    })
+  })
+
+
+  function optionClicked(){
+    switch(true){
+      case this.classList.contains('option1'):
+        alert( 'user clicked option1' );
+        break;
+      case this.classList.contains('option2'):
+        alert( 'user clicked option2' );
+        break;
+      case this.classList.contains('option3'):
+        alert( 'user clicked option3' );
+        break;
+    }
+  } */
+
+/*   theme.forEach(function (i) {
+  i.addEventListener('click', function() {
+    document.querySelector('.msg').innerHTML = i.innerHTML;
+  });
+}); */
+let themeSelections = document.querySelectorAll('.theme');
+
+/* themes.forEach(function (i) {
+  i.addEventListener('click', function() {
+    let themeSelection = innerHTML = i.innerHTML;
+    theme.classList.toggle(themeSelection);
+    console.log(themeSelection)
+  });
+});
+ */
+
+themeSelections.forEach((themeSelection, themeSelectionIndex) => {
+    themeSelection.addEventListener('click', (e) => {
+      e.preventDefault();
+        themeSelections.forEach((theme, themeIndex) => {   
+            console.log(themeSelection)    
+        // If button is for next step, add class to next step, else remove
+        if (themeSelectionIndex === themeIndex) {
+            theme.classList.add('activeFieldset');
+        } else {
+            theme.classList.remove('activeFieldset');
+        }
+      });
+    });
+  });
